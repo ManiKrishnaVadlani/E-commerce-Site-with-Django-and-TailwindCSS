f6cb07075b445f6e56d7f158e169eb70cbe9a377
ManiKrishnaVadlani <vadlanimani9999@gmail.com>
2025-01-13 22:15:39 +0530

    E

---
+++ ecommercesite/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html',
+    './products/templates/**/*.html',
+    './cart/templates/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
